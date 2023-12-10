c3d12050482034dace92718f10bf9491b5466201
Sygeman <sygeman@gmail.com>
2023-12-10 14:02:56 +0300

    upd bg

--- src/components/tree/background-dots.tsx
+++ src/components/tree/background-dots.tsx
@@ -1,9 +1,6 @@
 export const BackgroundDots = () => (
   <>
-    <div
-      className={`absolute inset-0 w-full h-full
-      bg-gradient-to-r from-teal-500 from-30% to-teal-700 to-90%`}
-    />
+    <div className="absolute inset-0 w-full h-full bg-teal-500" />
     <div
       className={`absolute inset-0 w-full h-full
       pattern-dots pattern-blue-500 pattern-bg-white pattern-size-2 pattern-opacity-20`}
